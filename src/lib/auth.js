7b43a0571ec2de3ddbcb079f234597ad1c5143dd
l1nky358 <prokotrob@gmail.com>
2026-04-12 23:53:19 +0700

    Update auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -31,7 +31,7 @@ export default async function configureAuth(app) {
   });
 
   // Выход
-  app.delete('/session', async (request, reply) => {
+  app.post('/session/delete', async (request, reply) => {
     request.session.destroy();
     reply.flash('success', 'Вы успешно вышли');
     return reply.redirect('/');
