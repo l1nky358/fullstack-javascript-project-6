13ff3bed35e18a3277d6f69c7bab98c0ad4d0767
l1nky358 <prokotrob@gmail.com>
2026-04-12 23:34:56 +0700

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,9 @@ import knex from 'knex';
 import { Model } from 'objection';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,6 +15,7 @@ const isTest = process.env.NODE_ENV === 'test';
 let knexConfig;
 
 if (isTest) {
+  // In-memory SQLite для тестов
   knexConfig = {
     client: 'sqlite3',
     connection: ':memory:',
@@ -23,8 +27,8 @@ if (isTest) {
       directory: path.join(__dirname, '../seeds'),
     },
   };
-}
- else if (isProduction) {
+} else if (isProduction) {
+  // PostgreSQL для продакшена
   knexConfig = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
@@ -33,8 +37,8 @@ if (isTest) {
       directory: path.join(__dirname, '../migrations'),
     },
   };
-}
- else {
+} else {
+  // SQLite для разработки
   knexConfig = {
     client: 'sqlite3',
     connection: {
